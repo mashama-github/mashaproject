d6c1aa52e66906b15573cd42a224425a725c2524
馬珮紋 <mpw8973@gmail.com>
2018-01-29 14:47:22 +0800

    SETTING
    CHANGE MYSQL

--- mashaproject/mashaproject/settings.py
+++ mashaproject/mashaproject/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mashmaproject',
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': 'mashma@localhost',
+        'PASSWORD': 'mashma',
         'HOST': 'localhost',
         
     }
